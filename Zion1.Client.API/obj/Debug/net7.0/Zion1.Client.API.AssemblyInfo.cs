40f8f85eb81d875063b12b800b82d602c6f95d35
Hoang Dinh <hoangdc@gmail.com>
2023-09-05 22:12:25 -0500

    update

--- Zion1.Client.API/obj/Debug/net7.0/Zion1.Client.API.AssemblyInfo.cs
+++ Zion1.Client.API/obj/Debug/net7.0/Zion1.Client.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zion1.Client.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0fa041d97cf7fa6ad243accd1d61640d3a70cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zion1.Client.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zion1.Client.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Zion1.Client.Component/obj/Debug/net7.0/Zion1.Client.Component.AssemblyInfo.cs
+++ Zion1.Client.Component/obj/Debug/net7.0/Zion1.Client.Component.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zion1.Client.Component")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9dee7fefa42182271ac5f1eabdf4f3ba2dc6b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0fa041d97cf7fa6ad243accd1d61640d3a70cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zion1.Client.Component")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zion1.Client.Component")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
