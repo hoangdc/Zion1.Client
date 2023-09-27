909370776b551169e814aa20ad9c14c445a6a371
Hoang Dinh <hoangdc@gmail.com>
2023-09-26 23:24:19 -0500

    refactor Client module

---
+++ Zion1.Client.Application/Queries/GetClientQuery.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Zion1.Client.Domain.Entities;
+using Zion1.Client.Application.Contracts;
+
+namespace Zion1.Client.Application.Queries
+{
+    public class GetClientQuery : IRequest<ClientInfo>
+    {
+        public int ClientId { get; set; } = 0;
+
+        public GetClientQuery(int clientId)
+        {
+            ClientId = clientId;
+        }
+
+        public class GetClientListQueryHandler : IRequestHandler<GetClientQuery, ClientInfo>
+        {
+            private readonly IClientQueryRepository _clientRepository;
+            public GetClientListQueryHandler(IClientQueryRepository clientRepository)
+            {
+                _clientRepository = clientRepository;
+            }
+            
+
+            public async Task<ClientInfo> Handle(GetClientQuery request, CancellationToken cancellationToken)
+            {
+                return await _clientRepository.GetByIdAsync(request.ClientId);
+            }
+            
+        }
+    }
+}
